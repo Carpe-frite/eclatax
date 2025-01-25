c14ad86f4fadec87f9283fd45464e28ca4abd4d4
Antoine Germon <antoine.germon@etu.unistra.fr>
2025-01-25 10:07:49 +0100

    Déplacement éléphant dans son propre script

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -2,3 +2,6 @@ extends Node
 
 var bubble_id = 0
 var bubble_array = []
+
+var elephant_x = 100
+var speed = 1000
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,21 +1,8 @@
 extends Node2D
 
-@onready var acteur_elephant = get_node("Monde/acteurs/elephant")
-
-#VARIABLES IMPORTANTES
-var elephant_x = 100
-var speed = 1000
-
 #CLASSES
 @onready var bubble = bubble_class
 
-func move_elephant(delta):
-	if Input.is_action_pressed("ui_kb_MoveElephantRight"):
-		elephant_x = elephant_x + speed * delta
-		acteur_elephant.set_global_position(Vector2(elephant_x,0))
-	if Input.is_action_pressed("ui_kb_MoveElephantLeft"):
-		elephant_x = elephant_x - speed * delta
-		acteur_elephant.set_global_position(Vector2(elephant_x,0))
 		
 func bubble_action():
 	if Input.is_action_just_released("ui_kb_Space"):
@@ -32,6 +19,5 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
-	move_elephant(delta)
 	bubble_action()
 	bubbles_go_up()
