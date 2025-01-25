9668cdbd6285752066e218df77edc31028b53d65
Antoine Germon <antoine.germon@etu.unistra.fr>
2025-01-25 10:59:59 +0100

    début séparation code

--- Scripts/bubble_class.gd
+++ Scripts/bubble_class.gd
@@ -1,3 +1,5 @@
+extends Node2D
+
 class_name bubble_class
 
 var bulle = preload("res://bulle.tscn")
@@ -10,9 +12,21 @@ func _init() -> void:
 	bubble_instance.set_global_position(Vector2(bubble_properties["x"], bubble_properties["y"]))
 	global.bubble_array.append(self)
 	var test = "oui"
-	
+
+func bubble_action():
+	if Input.is_action_just_released("ui_kb_Space"):
+		var new_bubble = bubble_class.new()
+		self.add_child(global.bubble_array[global.bubble_id].bubble_instance)
+		global.bubble_id += 1
+		
+func bubbles_go_up(delta):
+	for x in global.bubble_array:
+		x.bubble_properties["y"] = x.bubble_properties["y"] - global.speed_bubble * delta
+		x.bubble_instance.set_global_position(Vector2(x.bubble_properties["x"],x.bubble_properties["y"]))
+
 func _ready():
 	pass
 	
-func _process():
-	pass
+func _process(delta: float) -> void:
+	bubble_action()
+	bubbles_go_up(delta)
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -3,21 +3,8 @@ extends Node2D
 #CLASSES
 @onready var bubble = bubble_class
 
-		
-func bubble_action():
-	if Input.is_action_just_released("ui_kb_Space"):
-		var new_bubble = bubble.new()
-		self.add_child(global.bubble_array[global.bubble_id].bubble_instance)
-		global.bubble_id += 1
-		
-func bubbles_go_up(delta):
-	for x in global.bubble_array:
-		x.bubble_properties["y"] = x.bubble_properties["y"] - global.speed_bubble * delta
-		x.bubble_instance.set_global_position(Vector2(x.bubble_properties["x"],x.bubble_properties["y"]))
-
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
-	bubble_action()
-	bubbles_go_up(delta)
+	pass
