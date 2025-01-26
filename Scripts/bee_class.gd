788d8b0ebe876439da761cd4aecfeb3bc5ea86f2
Gaetan-Piou <g.piou@ludus-academie.com>
2025-01-26 11:49:16 +0100

    Added some powerups

--- Scripts/bee_class.gd
+++ Scripts/bee_class.gd
@@ -9,18 +9,22 @@ var delta_time = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	##Bee init
+	global.bee_speed = 20 # Default 20
+	global.bee_size = 0.6 # Default 0.6
+	global.bee_luck = 1
+	global.bee_x = 960
+	global.bee_y = 360
+	global.bee_cripique_active = true
+	
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	if global.checkIfAcquired(global.bee_powerups, "Precipiquation"):
-		global.bee_speed *= 2
+		global.bee_speed *= 1.5
 	if global.checkIfAcquired(global.bee_powerups, "Piquantesque"):
 		global.bee_size *= 1.75
 	if global.checkIfAcquired(global.bee_powerups, "Frenepique"):
-		global.bee_size *= 0.5
-		global.bee_speed *= 5
-	if global.checkIfAcquired(global.bee_powerups, "Flegmapique"):
-		global.speed_bubble *= 0.5
-	if global.checkIfAcquired(global.bee_powerups, "Expiqueration"):
-		global.timer *= 0.9
+		global.bee_size *= 0.7
+		global.bee_speed *= 3
 	
 	
 
@@ -30,14 +34,14 @@ func _process(delta: float) -> void:
 	
 	set_bee_size(global.bee_size)
 	# set_bee_speed()
+	bee.position.x = global.bee_x
+	bee.position.y = global.bee_y
 	
 	animated_bee.play("default")
 
 # Called whenever there is an input event
 func _input(event):
 	var border_spacing = 50
-	bee.position.x = global.bee_x
-	bee.position.y = global.bee_y
 	if(global.bee_x < border_spacing):
 		global.bee_x = border_spacing
 	if(global.bee_x > 1920 - border_spacing):
@@ -52,7 +56,7 @@ func _input(event):
 
 # Setters
 func set_bee_size(new_size):
-	animated_bee.scale = Vector2(new_size, new_size)
+	bee.scale = Vector2(new_size, new_size)
 
 func set_bee_speed(new_speed):
 	global.bee_speed = new_speed
--- round_timer.gd
+++ round_timer.gd
@@ -3,6 +3,18 @@ extends Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#Timer properties init
+	global.timer = 60
+	
+	if global.checkIfAcquired(global.elephant_powerups, "Probullession"):
+		global.speed_bubble *= 1.33
+	if global.checkIfAcquired(global.elephant_powerups, "Bulln-Out"):
+		global.timer *= 1.5
+	if global.checkIfAcquired(global.bee_powerups, "Flegmapique"):
+		global.speed_bubble *= 0.5
+	if global.checkIfAcquired(global.bee_powerups, "Expiqueration"):
+		global.timer *= 0.9
+	
 	self.set_wait_time(global.timer) # Replace with function body.
 
 
